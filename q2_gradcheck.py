d5dd29c45310fc9cfacbc65175f4d7aadb885472
vishalkg <vishal.gupta4081@gmail.com>
2017-03-09 20:15:38 -0500

    updated

--- q2_gradcheck.py
+++ q2_gradcheck.py
@@ -23,32 +23,35 @@ def gradcheck_naive(f, x):
         ### make sure you call random.setstate(rndstate) before calling f(x) each time, this will make it 
         ### possible to test cost functions with built in randomness later
         ### YOUR CODE HERE:
-        raise NotImplementedError
+        xh = np.zeros(x.shape)
+        xh[ix] = xh[ix]+h
+        random.setstate(rndstate)
+        f_xh, grad_xh = f(x+xh)
+        numgrad = (f_xh-fx)/h
         ### END YOUR CODE
 
         # Compare gradients
         reldiff = abs(numgrad - grad[ix]) / max(1, abs(numgrad), abs(grad[ix]))
         if reldiff > 1e-5:
-            print "Gradient check failed."
-            print "First gradient error found at index %s" % str(ix)
-            print "Your gradient: %f \t Numerical gradient: %f" % (grad[ix], numgrad)
+            print ("Gradient check failed.")
+            print ("First gradient error found at index %s" % str(ix))
+            print ("Your gradient: %f \t Numerical gradient: %f" % (grad[ix], numgrad))
             return
     
         it.iternext() # Step to next dimension
 
-    print "Gradient check passed!"
-
+    print ("Gradient check passed!")
 def sanity_check():
     """
     Some basic sanity checks.
     """
     quad = lambda x: (np.sum(x ** 2), x * 2)
 
-    print "Running sanity checks..."
+    print ("Running sanity checks...")
     gradcheck_naive(quad, np.array(123.456))      # scalar test
     gradcheck_naive(quad, np.random.randn(3,))    # 1-D test
     gradcheck_naive(quad, np.random.randn(4,5))   # 2-D test
-    print ""
+    print ("")
 
 def your_sanity_checks(): 
     """
@@ -57,9 +60,9 @@ def your_sanity_checks():
     This function will not be called by the autograder, nor will
     your additional tests be graded.
     """
-    print "Running your sanity checks..."
+    print ("Running your sanity checks...")
     ### YOUR CODE HERE
-    raise NotImplementedError
+    #raise NotImplementedError
     ### END YOUR CODE
 
 if __name__ == "__main__":
